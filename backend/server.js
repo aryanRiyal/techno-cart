933f6faf689266dc8713d91d71a7a908ec5a5f38
Aryan Riyal <aryan13.riyal@gmail.com>
2022-05-26 20:04:59 +0530

    project initiation

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const express = require('express');
+const connectDB = require('./config/db');
+
+connectDB();
+
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, ()=> {
+  console.log(`Server Running on ${PORT}`);
+});
