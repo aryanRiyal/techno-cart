4eb9c537c5f7c9a6279d573377c267fabe73bf60
Sidharth513 <sharmasidharth513@gmail.com>
2022-05-31 00:28:02 +0530

    React Added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
+import{useState} from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 //Screens
@@ -9,21 +10,29 @@ import CartScreen from './screens/CartScreen';
 
 //Components
 import Navbar from './components/Navbar';
+import BackDrop from './components/BackDrop';
+import SideDrawer from './components/SideDrawer';
 
 function App() {
+
+  const [sideToggle, setSideToggle] = useState(false);
+
   return (
     <Router>
       {/* Navbar */}
-      <Navbar />
-
-      {/* SideDrawer */}
-      {/* Backdrop */}
+      <Navbar click={() => setSideToggle(true)}/>
+      {/* SlideDrawer*/ }
+      <SideDrawer show={sideToggle} click={() => setSideToggle(false)}/>
+      {/* BackDrawer*/ }
+      <BackDrop show={sideToggle} click={() => setSideToggle(false)}/>
 
       <main>
         <Routes>
-          <Route exact path = "/" component ={ HomeScreen }/>
-          <Route exact path = "/product/:id" component ={ ProductScreen } />
-          <Route exact path = "/cart" component ={ CartScreen } />
+          <Route exact path = "/" element ={ <HomeScreen/> }/>
+          <Route exact path = "/product/:id" element ={ <ProductScreen />} />
+          {/* <Route exact path = "/cart" component ={ CartScreen } /> */}
+        
+          <Route exact path = "/cart" element ={ <CartScreen />} />
         </Routes>
       </main>
 
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,7 +1,7 @@
 import './Navbar.css';
 import {Link} from 'react-router-dom';
 
-const Navbar = () => {
+const Navbar = ({click}) => {
   return (
     <nav className="navbar">
       {/* logo */}
@@ -10,13 +10,14 @@ const Navbar = () => {
       </div>
       
       {/* links */}
-      <ul>
+      <ul className="navbar__links">
         <li>
-          <Link to="/cart">
+          <Link to="/cart" className="cart__links">
             { /* Icon */ }
             <i className="fa-solid fa-cart-shopping"></i>
-            Cart
-            <span className="cart__badge">0</span>
+            <span> Cart
+            <span className="cartlogo__badge">0</span></span>
+           
           </Link>
         </li>
         <li>
@@ -27,7 +28,7 @@ const Navbar = () => {
       </ul>
 
       {/* hamburger menu */}
-      <div className="hamburger__menu">
+      <div className="hamburger__menu" onClick={click}>
         <div></div>
         <div></div>
         <div></div>
