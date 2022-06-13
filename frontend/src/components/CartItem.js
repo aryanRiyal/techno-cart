e6d9c1f23b7aa7415d68e774e6c6b15a246272d6
Aryan Riyal <aryan13.riyal@gmail.com>
2022-06-13 23:59:54 +0530

    redux

--- frontend/src/components/CartItem.js
+++ frontend/src/components/CartItem.js
@@ -8,7 +8,7 @@ const CartItem = () => {
           <img src="https://images.unsplash.com/photo-1606813907291-d86efa9b94db?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80" alt=""/>
         </div>
 
-        <Link to={'/product/${111}'} className="cartitem__name">
+        <Link to={`/product/${111}`} className="cartitem__name">
             <p>Product 1</p>
         </Link>
 
@@ -27,4 +27,4 @@ const CartItem = () => {
     );
 }
 
-export default CartItem
+export default CartItem;
